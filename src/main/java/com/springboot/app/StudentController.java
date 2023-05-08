8bfaff203c994e7c8b0fd54cbf3343d6cabb7e7f
Asadujjaman <41225800+Asadujjaman47@users.noreply.github.com>
2023-05-08 15:46:31 +0600

    Spring Boot REST API with Path Variable - @PathVariable

--- src/main/java/com/springboot/app/StudentController.java
+++ src/main/java/com/springboot/app/StudentController.java
@@ -1,7 +1,11 @@
 package com.springboot.app;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class StudentController {
@@ -10,5 +14,25 @@ public class StudentController {
     @GetMapping("/student")
     public Student getStudent() {
         return new Student("Md. ", "Asad");
+    }
+
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Md. ", "Asad"));
+        students.add(new Student("B ", "E"));
+        students.add(new Student("C ", "F"));
+        students.add(new Student("D", "G"));
+
+        return students;
+    }
+
+    // http://localhost:8080/student/mohammad/ali
+    // @PathVariable annotation
+    @GetMapping("/student/{firstName}/{lastName}")
+    public Student studentPathVariable(@PathVariable("firstName") String firstName, @PathVariable("lastName") String lastName) {
+        return new Student(firstName, lastName) ;
     }
 }
